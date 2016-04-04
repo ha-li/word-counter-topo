8bc92906f74d4999759e7f9cdd7b146e3b35a081
ha-li <hao.lieu@gmail.com>
2016-04-04 08:44:18 -0700

    Add my own topo

---
+++ src/main/java/com/gecko/topo/bolts/WordCounter.java
@@ -0,0 +1,45 @@
+package com.gecko.topo.bolts;
+
+import backtype.storm.task.TopologyContext;
+import backtype.storm.topology.BasicOutputCollector;
+import backtype.storm.topology.OutputFieldsDeclarer;
+import backtype.storm.topology.base.BaseBasicBolt;
+import backtype.storm.tuple.Tuple;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by hlieu on 04/3/16.
+ */
+public class WordCounter extends BaseBasicBolt {
+    Integer id;
+    String name;
+    Map<String, Integer> counters;
+
+    public void cleanup() {
+        System.out.println("-- Word Counter [" + name + "-" + id + "] --");
+        for(Map.Entry<String, Integer> entry : counters.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
+    }
+
+    public void prepare(Map stormConf, TopologyContext context) {
+        this.counters = new HashMap<String, Integer>();
+        this.name = context.getThisComponentId();
+        this.id = context.getThisTaskId();
+    }
+
+    public void declareOutputFields(OutputFieldsDeclarer declarer) {}
+
+    public void execute(Tuple input, BasicOutputCollector collector) {
+        String str = input.getString(0);
+
+        if(!counters.containsKey(str)) {
+            counters.put(str, 1);
+        } else {
+            // increment the current value
+            counters.put(str, counters.get(str) + 1);
+        }
+    }
+}
---
+++ src/main/java/com/gecko/topo/spouts/WordReader.java
@@ -0,0 +1,61 @@
+package com.gecko.topo.spouts;
+
+import backtype.storm.spout.SpoutOutputCollector;
+import backtype.storm.task.TopologyContext;
+import backtype.storm.topology.OutputFieldsDeclarer;
+import backtype.storm.topology.base.BaseRichSpout;
+import backtype.storm.tuple.Fields;
+import backtype.storm.tuple.Values;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Map;
+
+/**
+ * Created by hlieu on 04/3/16.
+ */
+public class WordReader extends BaseRichSpout {
+    private SpoutOutputCollector collector;
+    private FileReader fileReader;
+    private boolean completed = false;
+
+    public void ack(Object msgId) {System.out.println("OK " + msgId);}
+    public void close() {}
+    public void fail(Object msgId) {System.out.println("Failed " + msgId);}
+
+    public void nextTuple() {
+        if(completed) {
+            try {
+                Thread.sleep(1000);
+            }catch(InterruptedException e) {
+
+            }
+            return;
+        }
+
+        String str;
+        BufferedReader reader = new BufferedReader(fileReader);
+        try {
+            // read all lines
+            while( (str = reader.readLine()) != null) {
+                this.collector.emit(new Values(str), str);
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        } finally {
+            completed = true;
+        }
+    }
+
+    public void open(Map conf, TopologyContext context, SpoutOutputCollector collector) {
+        try {
+            this.fileReader = new FileReader(conf.get("wordsfile").toString());
+        } catch (FileNotFoundException fe) {
+            throw new RuntimeException(fe);
+        }
+        this.collector = collector;
+    }
+
+    public void declareOutputFields(OutputFieldsDeclarer declarer) {declarer.declare(new Fields("line"));}
+}
